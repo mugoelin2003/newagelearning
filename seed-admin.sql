51cf3c690b88c2ffd7728a12fcb29bf701ab8de6
mugoelin2003 <mu.goelin2003@gmail.com>
2026-06-18 16:12:38 +0530

    Update seed-admin.sql

--- seed-admin.sql
+++ seed-admin.sql
@@ -3,10 +3,10 @@
 --   psql "<YOUR EXTERNAL DATABASE URL>" -f seed-admin.sql
 --
 -- Then log in to /admin with:
---   mobile:   9800000000
+--   mobile:   9937049949
 --   password: NewAge@2026
 -- (change the mobile below if you like; the password matches the hash)
 
 INSERT INTO users (role, name, mobile, password_hash)
-VALUES ('admin', 'Admin', '9800000000',
+VALUES ('admin', 'Admin', '9937049949',
         '$2b$10$sjA42CFHVIYMjLBCe4Vsk.87ofXl9nGQsO/s.HTC5/HYl0Ne.p2Lu');
